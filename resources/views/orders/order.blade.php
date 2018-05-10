6a063595c1f361d4bbc79c55426c4eda99fb5320
remcodezwart <remcodezwart1997@gmail.com>
2018-05-10 20:05:28 +0200

    fixed a minor bug

--- resources/views/orders/order.blade.php
+++ resources/views/orders/order.blade.php
@@ -5,8 +5,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">order {{ $order->id }}</div>
-				@if (!empty($order))
+                @if (!empty($order))
+                <div class="panel-heading">
+                    order {{ $order->id }}</div>
                     <div class="panel-heading">
                         status: {{ $order->status }} <br>
                         datum besteld: {{ $order->date_order }} <br>
